3fb389b4894f8e646c466ed173b24ac0f18c49c5
Prince Sharma <116869578+PrinceSharma-afk@users.noreply.github.com>
2026-08-17 16:25:46 +0530

    Update content.js
    Add interactive headings view

--- content.js
+++ content.js
@@ -1,13 +1,31 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+
     if (message.type === "getSiteInfo") {
-        sendResponse({
-            url: window.location.href,
-            siteName: window.location.hostname,
-            title:document.title,
-            links:document.querySelectorAll('a').length,
-            images:document.querySelectorAll('img').length,
-            headings: document.querySelectorAll("h1, h2, h3, h4, h5, h6").length,
-            forms: document.querySelectorAll("form").length
+
+        const headingElements = document.querySelectorAll(
+            "h1, h2, h3, h4, h5, h6"
+        );
+
+        const headings = Array.from(headingElements).map((heading, index) => {
+            return {
+                id: index,
+                text: heading.innerText,
+                level: heading.tagName
+            };
         });
+
+        const data = {
+            siteName: window.location.hostname,
+            url: window.location.href,
+            title: document.title,
+            links: document.querySelectorAll("a").length,
+            images: document.querySelectorAll("img").length,
+            headings: headings.length,
+            headingList:headings,
+            forms: document.querySelectorAll("form").length
+        };
+
+        sendResponse(data);
     }
+
 });
