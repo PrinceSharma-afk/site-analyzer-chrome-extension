ac04a16fd121382f989d81d836f6eccb9bfc4a34
Prince Sharma <116869578+PrinceSharma-afk@users.noreply.github.com>
2026-08-18 00:57:24 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,8 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
-    if (message.type === "getSiteInfo") {
+    if (message.type === "analyzePage") {
+
+        // Headings
 
         const headingElements = document.querySelectorAll(
             "h1, h2, h3, h4, h5, h6"
@@ -14,16 +16,66 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
             };
         });
 
+
+        // Images
+
+        const imgEle = document.querySelectorAll("img");
+
+        const imgList = Array.from(imgEle).map((img, index) => {
+            return {
+                id: index,
+                src: img.src,
+                alt: img.alt
+            };
+        });
+
+
+        // Links
+
+        const links = document.querySelectorAll("a");
+
+        const linkList = Array.from(links).map((link, index) => {
+            return {
+                id: index,
+                text: link.innerText,
+                url: link.href
+            };
+        });
+
+
+        // Forms
+
+        const forms = document.querySelectorAll("form");
+
+        const formList = Array.from(forms).map((form, index) => {
+            return {
+                id: index,
+                action: form.action,
+                method: form.method.toUpperCase()
+            };
+        });
+
+
+        // Page data
+
         const data = {
             siteName: window.location.hostname,
             url: window.location.href,
             title: document.title,
-            links: document.querySelectorAll("a").length,
-            images: document.querySelectorAll("img").length,
+
+            links: links.length,
+            linkList: linkList,
+
+            images: imgEle.length,
+            imageList: imgList,
+
             headings: headings.length,
-            headingList:headings,
-            forms: document.querySelectorAll("form").length
+            headingList: headings,
+
+            forms: forms.length,
+            formList: formList
         };
+
 
         sendResponse(data);
     }
